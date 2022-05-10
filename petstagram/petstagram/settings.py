30e1950060253768323425d8f749f6047719da87
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-10 20:19:07 +0300

    configuration for static and media files

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -120,7 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, "static_my_project"),
+    ]
+
+STATIC_ROOT= os.path.join(BASE_DIR, "static_cdn", "static_root")
+
+
+MEDIA_URL= '/media/'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, "static_cdn", "media_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
