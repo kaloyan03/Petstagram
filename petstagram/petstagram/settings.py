a8eafbf4e410621fb178f5e1606d49a9c984d408
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-10 20:45:37 +0300

    apps added to INSATLLED_APPS

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # APPS
+    'petstagram.petstagram.home',
+    'petstagram.petstagram.auth_app',
 ]
 
 MIDDLEWARE = [
