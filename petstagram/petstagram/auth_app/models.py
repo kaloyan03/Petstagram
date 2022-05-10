1ed624edb79543330f8a9f2720a53c94ffa6420f
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-10 22:29:05 +0300

    extended base user model

--- petstagram/petstagram/auth_app/models.py
+++ petstagram/petstagram/auth_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
+from petstagram.auth_app.managers import PetstagramUserManager
+
+
+class PetstagramUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    objects = PetstagramUserManager()
+
+    USERNAME_FIELD = "email"
