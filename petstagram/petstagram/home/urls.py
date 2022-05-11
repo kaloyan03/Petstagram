8ba3173879e8b776a0878fc91d38a06c6ee1ca02
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-11 12:37:09 +0300

    fixed name of the url

--- petstagram/petstagram/home/urls.py
+++ petstagram/petstagram/home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from petstagram.home import views as home_views
 
 urlpatterns = (
-    path('', home_views.HomeView.as_view(), name='home page view'),
+    path('', home_views.HomeView.as_view(), name='home page'),
 )
