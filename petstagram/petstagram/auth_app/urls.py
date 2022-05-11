ccbb854e5ca27eec08686caa239c0b561ce202ed
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-11 12:34:35 +0300

    implemented view for sign up and added it to the urls

--- petstagram/petstagram/auth_app/urls.py
+++ petstagram/petstagram/auth_app/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 
+from petstagram.auth_app.views import SignUpView
 
 urlpatterns = (
-
+    path('sign-up/', SignUpView.as_view(), name='sign up view'),
 )
--- petstagram/petstagram/auth_app/views.py
+++ petstagram/petstagram/auth_app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views import generic as views
+
+from petstagram.auth_app.forms import SignUpForm
+from petstagram.profiles.forms import CreateProfileForm
+
+
+class SignUpView(views.TemplateView):
+    template_name = 'profile_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['sign_up_form'] = SignUpForm()
+        context['profile_form'] = CreateProfileForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        profile_form = CreateProfileForm(request.POST, request.FILES)
+        sign_up_form = SignUpForm(request.POST)
+
+        if profile_form.is_valid() and sign_up_form.is_valid():
+            user = sign_up_form.save()
+            profile = profile_form.save(commit=False)
+
+            profile.user = user
+
+            profile.save()
+
+            return redirect('home page')
+
+        return render(request, 'profile_create.html', context={'profile_form': profile_form, 'sign_up_form': sign_up_form})
+
+
+
