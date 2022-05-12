8315a7ff173b4255d24a1ba5d96d31464d46bacb
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-12 18:16:44 +0300

    written skeletons of views in the profile app

--- petstagram/petstagram/profiles/urls.py
+++ petstagram/petstagram/profiles/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = (
+from django.urls import path
+from petstagram.profiles import views as profile_views
 
+urlpatterns = (
+    path('profile/<int:pk>/', profile_views.ShowProfileView.as_view(), name='show profile view'),
+    path('edit-profile/<int:pk>/', profile_views.EditProfileView.as_view(), name='edit profile view'),
 )
--- petstagram/petstagram/profiles/views.py
+++ petstagram/petstagram/profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic as views
 
 # Create your views here.
+class ShowProfileView(views.TemplateView):
+    pass
+
+class EditProfileView(views.UpdateView):
+    pass
+
