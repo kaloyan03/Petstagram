3942612b8a7dc8c5c1a79df383bac6174e5d7cb9
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-12 18:17:07 +0300

    written skeletons of views in the pets app

--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from django.views import generic as views
+
 # Create your views here.
+class AddPetView(views.CreateView):
+    pass
+
+
+class EditPetView(views.UpdateView):
+    pass
+
+
+class DeletePetView(views.DeleteView):
+    pass
+
+
+class AddPetPhotoView(views.CreateView):
+    pass
+
+
+class EditPetPhotoView(views.UpdateView):
+    pass
+
