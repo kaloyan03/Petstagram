fd75dd11b792c0fae7877fc8df8e9110161c4613
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-12 18:28:01 +0300

    urls in the pets app added

--- petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/pets/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = (
+from django.urls import path
+from petstagram.pets import views as pets_views
 
+
+urlpatterns = (
+    path('add/', pets_views.AddPetView.as_view(), name='add pet view'),
+    path('edit/<int:pk>', pets_views.EditPetView.as_view(), name='edit pet view'),
+    path('delete/<int:pk>', pets_views.DeletePetView.as_view(), name='delete pet view'),
+    path('add-photo/', pets_views.AddPetPhotoView.as_view(), name='add pet photo view'),
+    path('edit-photo/', pets_views.EditPetPhotoView.as_view(), name='edit pet photo view'),
 )
