77f144dbf7c4bccdea4de8d3383c374f94a2d4b0
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-13 09:28:15 +0300

    tagged_pets field changed - ForeignKey to Pet; str method to Pet model implemented; functionality when you delete PetPhoto object to be deleting image file implemented

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.validators import MinLengthValidator
 from django.db import models
 
@@ -15,7 +17,7 @@ class Pet(models.Model):
     )
 
     TYPE_CHOICES_MAX_LENGTH = max([len(x) for x, _ in TYPE_CHOICES])
-    TYPE_CHOICES_MIN_LENGTH = max([len(x) for x, _ in TYPE_CHOICES])
+    TYPE_CHOICES_MIN_LENGTH = min([len(x) for x, _ in TYPE_CHOICES])
 
     name = models.CharField(
         max_length=NAME_MAX_LENGTH,
@@ -29,9 +31,12 @@ class Pet(models.Model):
         )
     )
 
-    date_of_birth = models.DateTimeField(
+    date_of_birth = models.DateField(
 
     )
+
+    def __str__(self):
+        return f"{self.name} - {self.type}"
 
 
 class PetPhoto(models.Model):
@@ -40,8 +45,10 @@ class PetPhoto(models.Model):
         upload_to='pet_photos',
     )
 
-    tagged_pets = models.ManyToManyField(
+    tagged_pets = models.ForeignKey(
         Pet,
+        on_delete=models.CASCADE,
+        default=1
     )
 
     description = models.TextField(
@@ -52,4 +59,9 @@ class PetPhoto(models.Model):
         auto_now_add=True,
     )
 
+    def delete(self, using=None, keep_parents=False):
+        if os.path.isfile(self.photo.path):
+            os.remove(self.photo.path)
+
+        super().delete()
 
