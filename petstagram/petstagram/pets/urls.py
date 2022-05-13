ed663e712da1ff600ff727dd42d3379a32b63ae5
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-13 09:29:13 +0300

    Pet model views added and urls configurated

--- petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/pets/urls.py
@@ -3,7 +3,9 @@ from petstagram.pets import views as pets_views
 
 
 urlpatterns = (
+    path('', pets_views.ListPetsView.as_view(), name='list pets view'),
     path('add/', pets_views.AddPetView.as_view(), name='add pet view'),
+    path('<int:pk>/', pets_views.DetailsPetView.as_view(), name='details pet view'),
     path('edit/<int:pk>', pets_views.EditPetView.as_view(), name='edit pet view'),
     path('delete/<int:pk>', pets_views.DeletePetView.as_view(), name='delete pet view'),
     path('add-photo/', pets_views.AddPetPhotoView.as_view(), name='add pet photo view'),
--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -1,24 +1,69 @@
+from datetime import datetime
+
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from django.views import generic as views
 
 # Create your views here.
+from petstagram.pets.forms import CreatePetForm, AddPetPhotoForm
+from petstagram.pets.models import Pet, PetPhoto
+
+
 class AddPetView(views.CreateView):
-    pass
+    template_name = 'pet_create.html'
+    model = Pet
+    form_class = CreatePetForm
 
 
 class EditPetView(views.UpdateView):
-    pass
+    template_name = 'pet_edit.html'
 
 
 class DeletePetView(views.DeleteView):
-    pass
+    template_name = 'pet_delete.html'
+
+class ListPetsView(views.ListView):
+    template_name = 'dashboard.html'
+    model = Pet
+    context_object_name = 'pets'
+
+    def get_queryset(self):
+        pets = Pet.objects.all()
+        for pet in pets:
+            pet.age = datetime.now().year - pet.date_of_birth.year
+
+        return pets
 
 
 class AddPetPhotoView(views.CreateView):
-    pass
+    template_name = 'photo_create.html'
+    model = PetPhoto
+    success_url = reverse_lazy('list pets view')
+    form_class = AddPetPhotoForm
+
 
 
 class EditPetPhotoView(views.UpdateView):
-    pass
+    template_name = 'photo_edit.html'
+
+class DetailsPetView(views.TemplateView):
+    template_name = 'photo_details.html'
+    model = Pet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pet_id = self.kwargs['pk']
+        pet = Pet.objects.get(id=pet_id)
+        pet_photo = PetPhoto.objects.get(tagged_pets_id=pet.id)
+        pet.photo = pet_photo.photo
+        pet.description = pet_photo.description
+        pet.age = datetime.now().year - pet.date_of_birth.year
+        context['pet'] = pet
+
+
+
+        return context
+
+
 
