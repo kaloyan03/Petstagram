b0cef200587104e9093537592b4a70b627e58959
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-13 09:29:34 +0300

    Profile model views added and urls configurated

--- petstagram/petstagram/profiles/urls.py
+++ petstagram/petstagram/profiles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from petstagram.profiles import views as profile_views
 
 urlpatterns = (
-    path('profile/<int:pk>/', profile_views.ShowProfileView.as_view(), name='show profile view'),
+    path('<int:pk>/', profile_views.ShowProfileView.as_view(), name='show profile view'),
     path('edit-profile/<int:pk>/', profile_views.EditProfileView.as_view(), name='edit profile view'),
+    path('delete-profile/<int:pk>/', profile_views.DeleteProfileView.as_view(), name='delete profile view'),
 )
--- petstagram/petstagram/profiles/views.py
+++ petstagram/petstagram/profiles/views.py
@@ -1,10 +1,23 @@
+
 from django.shortcuts import render
 from django.views import generic as views
 
 # Create your views here.
-class ShowProfileView(views.TemplateView):
-    pass
+from petstagram.profiles.models import Profile
+
+
+class ShowProfileView(views.DetailView):
+    model = Profile
+    template_name = 'profile_details.html'
+    context_object_name = 'profile'
+
+
 
 class EditProfileView(views.UpdateView):
     pass
+
+
+class DeleteProfileView(views.DeleteView):
+    pass
+
 
